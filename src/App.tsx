363ee24eed5c8db47c3923df8a94aac6b8a0488b
amanpreetchatha <amanpreetchatha99@gmail.com>
2026-05-16 05:09:00 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,57 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [exitDate,setExitDate] = useState(new Date());
   const [enterDate, setEnterDate] = useState(new Date());
 
   return (
-    <>
-      <header>
-        <h3>Select an option: </h3>
-        <button
-          type="button"
-          className="exit"
-          onClick={() => setExitDate(new Date())}
-        >
-          Exited Canada
-        </button>
+    <html>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet"/>
+      </head>
+      <body>
+        
+      
+        <section>
+          <h3>Select an option: </h3>
+          <button
+            type="button"
+            className="exit"
+            onClick={() => setExitDate(new Date())}
+          >
+            Exited Canada
+          </button>
 
-        <button
-          type="button"
-          className="enter"
-          onClick={() => setEnterDate(new Date())}
-        >
-          Entered Canada
-        </button>
-      </header>
-      <section>
-        <h3>Travel history</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Exit Date</th>
-              <th>Enter Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{exitDate.toLocaleDateString()}</td>
-              <td>{enterDate.toLocaleDateString()}</td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-    </>
+          <button
+            type="button"
+            className="enter"
+            onClick={() => setEnterDate(new Date())}
+          >
+            Entered Canada
+          </button>
+        </section>
+        <section>
+          <h3>Travel history</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Exit Date</th>
+                <th>Enter Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{exitDate.toLocaleDateString()}</td>
+                <td>{enterDate.toLocaleDateString()}</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </body>
+    </html>
   )
 }
 
